03ae71e84b66871628fcdfac6b7d2c821f3826ed
Bharat <yadavbharat71@gmail.com>
2025-06-20 15:08:00 +0530

    model calss implemented

--- lib/database/db_heleper.dart
+++ lib/database/db_heleper.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:todo_app/database/model/todo_model.dart';
 
 class DbHelper {
   DbHelper._();
@@ -16,6 +17,7 @@ class DbHelper {
   static const CREATED_AT = "created_at";
   static const TITLE = "title";
   static const IS_DONE = "is_done";
+  static const CATEGORY = "category";
 
   Database? database;
 
@@ -38,54 +40,79 @@ class DbHelper {
       $ID INTEGER PRIMARY KEY AUTOINCREMENT,
       $CREATED_AT Text NOT NULL,
       $TITLE TEXT NOT NULL,
-      $IS_DONE  INTEGER NOT NULL
-      
+      $IS_DONE  INTEGER NOT NULL,
+      $CATEGORY TEXT NOT NULL
       )
 
       ''');
     });
   }
 
-  Future<List<Map<String, dynamic>>> fetchTodo() async {
+  Future<List<TodoModel>> fetchTodo({String? categorName}) async {
     var db = await initDB();
-    return await db.query(TABLE_NAME);
+    List<Map<String, dynamic>> todoMapData = [];
+
+    if (categorName != null && categorName.isNotEmpty) {
+      todoMapData = await db
+          .query(TABLE_NAME, where: "$CATEGORY=?", whereArgs: [categorName]);
+    } else {
+      todoMapData = await db.query(TABLE_NAME);
+    }
+    List<TodoModel> todoList = [];
+    for (int i = 0; i < todoMapData.length; i++) {
+      TodoModel todoModel = TodoModel.fromMap(todoMapData[i]);
+      todoList.add(todoModel);
+    }
+    return todoList;
   }
 
-  Future<bool> addTodo(String title) async {
+  Future<bool> addTodo({required TodoModel todoModel}) async {
     var db = await initDB();
-    int rowEffected = await db.insert(
-      TABLE_NAME,
-      {
-        TITLE: title,
-        CREATED_AT: DateTime.now().toIso8601String(),
-        IS_DONE: 0,
-      },
-    );
+    int rowEffected = await db.insert(TABLE_NAME, todoModel.toMap());
     return rowEffected > 0;
   }
 
-  Future<bool> updateTodo({required int id, required String title}) async {
+  Future<bool> updateTodo({required TodoModel todoModel}) async {
     var db = await initDB();
-    int rowEffected = await db.update(TABLE_NAME, {TITLE: title},
-        where: "$ID=?", whereArgs: [id]);
+    int rowEffected = await db.update(
+        TABLE_NAME, {TITLE: todoModel.title, CATEGORY: todoModel.category},
+        where: "$ID=?", whereArgs: [todoModel.id]);
     return rowEffected > 0;
   }
 
-  Future<bool> isTodoCompleted({required int id, required bool isDone}) async {
+  Future<bool> isTodoCompleted({required TodoModel todoModel}) async {
     var db = await initDB();
-    int rowEffected = await db.update(TABLE_NAME, {IS_DONE: isDone ? 1 : 0},
-        where: "$ID=?", whereArgs: [id]);
+    int rowEffected = await db.update(
+        TABLE_NAME, {IS_DONE: todoModel.isCompleted},
+        where: "$ID=?", whereArgs: [todoModel.id]);
 
     return rowEffected > 0;
   }
 
-  Future<bool> deleteTodo(int id) async {
+  Future<bool> deleteTodo({required TodoModel todoModel}) async {
     var db = await initDB();
     int rowEffected = await db.delete(
       TABLE_NAME,
       where: '$ID = ?',
-      whereArgs: [id],
+      whereArgs: [todoModel.id],
     );
     return rowEffected > 0;
+  }
+
+  Future<Map<String, dynamic>> fetchCounts() async {
+    var db = await initDB();
+    var allTodoCount = await db.query(TABLE_NAME);
+
+    var businessCount = await db
+        .query(TABLE_NAME, where: "$CATEGORY=?", whereArgs: ["business"]);
+
+    var personlCount = await db
+        .query(TABLE_NAME, where: "$CATEGORY=?", whereArgs: ["personl"]);
+
+    return {
+      "businessCount": businessCount.length,
+      "personalCount": personlCount.length,
+      "allCount": allTodoCount.length,
+    };
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/cubit/todo_cubit.dart';
-import 'package:todo_app/provider/todo_provider.dart';
+
 import 'package:todo_app/view/screen/todo_list_screen.dart';
 
 void main() {
@@ -17,8 +17,6 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         BlocProvider<TodoCubit>(create: (context) => TodoCubit()),
-        ChangeNotifierProvider<TodoProvider>(
-            create: (context) => TodoProvider())
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
